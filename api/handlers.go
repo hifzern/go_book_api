d1b5280c36b1c363aa23190ed61f0089e1ea6e24
hifzern <hafizhadhi27@gmail.com>
2025-08-21 10:52:18 +0700

    add handler

--- api/handlers.go
+++ api/handlers.go
@@ -11,4 +11,53 @@ import (
 
 var DB *gorm.DB
 
-func InitDB
+func InitDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to connect to database : ", err)
+	}
+
+	dsn := os.Getenv("DB_URL")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database : ", err)
+	}
+
+	//migrate the schema
+	if err := DB.AutoMigrate(&Book{}); err != nil {
+		log.Fatal("Failed to migrate schema : ", err)
+	}
+}
+
+func CreateBook(c *gin.Context) {
+	var book Book
+
+	//bind the request body
+	if err := c.ShouldBindJSON(&book); err != nil {
+		ResponseJSON(c, http.StatusBadRequest, "Invalid input", nil) 
+		return
+	}
+	DB.Create(&book)
+	ResponseJSON(c, http.StatusAccepted, "Book created successfully", book)
+}
+
+func GetBooks(c *gin.Context) {
+	var books []Book
+	DB.Find(&books)
+	ResponseJSON(c, http.StatusOK, "Books retrieved successfully", books)
+}
+
+func GetBook(c *gin.Context) {
+	var book Book
+	if err := DB.First(&book, c.Param("id")).Error; err != nil {
+		ResponseJSON(c, http.StatusNotFound, "Book not found", nil)
+		return
+	}
+	ResponseJSON(c, http.StatusOK, "Book retrieved successfully", book)
+}
+
+func UpdateBook(c *gin.Context) {
+	var book Book
+	if err := DB
+}
