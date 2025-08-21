f1fa2f0738c79576ee4a1c76415a8eb862eb5e2a
hifzern <hafizhadhi27@gmail.com>
2025-08-21 10:40:13 +0700

    add model and handlers

---
+++ api/handlers.go
@@ -0,0 +1,14 @@
+package api
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"os"
+)
+
+var DB *gorm.DB
+
+func InitDB
