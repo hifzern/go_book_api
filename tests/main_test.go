c58c53b845e8ddf6e100b9ba022cfd2c644c96cf
hifzern <hafizhadhi27@gmail.com>
2025-08-25 09:53:54 +0700

    add unit test

--- tests/main_test.go
+++ tests/main_test.go
@@ -2,7 +2,9 @@ package tests
 
 import (
 	"go_book_api/api"
+	"testing"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -11,6 +13,18 @@ func setupTestDB() {
 	var err error
 	api.DB, err = gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
 	if err != nil {
-
+		panic("failed to connect to database")
 	}
+	api.DB.AutoMigrate(&api.Book{})
+}
+
+func addBook() api.Book {
+	book := api.Book{Title: "Raja Solo", Author: "Wowok Gendut", Year: 2023}
+	api.DB.Create(&book)
+	return book
+}
+
+func TestCreateBook(t *testing.T) {
+	setupTestDB()
+	router := gin.Default()
 }
