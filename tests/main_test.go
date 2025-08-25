328600a8dfbe5675ded4cfc16bd302c37f6ba3d9
hifzern <hafizhadhi27@gmail.com>
2025-08-25 09:17:12 +0700

    init

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,1 +1,16 @@
 package tests
+
+import (
+	"go_book_api/api"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func setupTestDB() {
+	var err error
+	api.DB, err = gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+
+	}
+}
