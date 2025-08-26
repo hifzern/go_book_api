1a9ff4d094b56b8b48fa40c710550c89cb3f8073
hifzern <hafizhadhi27@gmail.com>
2025-08-26 14:20:36 +0700

    add unit test testcreatebook

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,7 +1,11 @@
 package tests
 
 import (
+	"bytes"
+	"encoding/json"
 	"go_book_api/api"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -27,4 +31,25 @@ func addBook() api.Book {
 func TestCreateBook(t *testing.T) {
 	setupTestDB()
 	router := gin.Default()
+	router.POST("/book", api.CreateBook)
+
+	book := api.Book{
+		Title:  "Raja Gorong-gorong",
+		Author: "Girban",
+		Year:   2023,
+	}
+	jsonValue, _ := json.Marshal(book)
+	req, _ := http.NewRequest("POST", "/book", bytes.NewBuffer(jsonValue))
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	if status := w.Code; status != http.StatusCreated {
+		t.Errorf("Expected status %d got %d", http.StatusCreated, status)
+	}
+	var response api.JsonResponse
+	json.NewDecoder(w.Body).Decode(&response)
+
+	if response.Data == nil {
+		t.Errorf("Expected book data got nil")
+	}
 }
